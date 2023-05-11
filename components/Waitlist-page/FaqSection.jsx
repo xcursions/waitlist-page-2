3b5a1da08c030c3210272ba8e067bbda0682d1ec
Adewale shittu <sheriffhardewale@gmail.com>
2023-05-11 23:52:30 +0100

    fixed faq

--- components/Waitlist-page/FaqSection.jsx
+++ components/Waitlist-page/FaqSection.jsx
@@ -23,10 +23,10 @@ const FaqSection = () => {
     viewport={{ once: true }}
       whileInView={"show"}
       variants={container}
-    id='faq-section' className='duration-300 transition-all m-auto w-[90%] pt-[180px] md:pt-[100px] lg:pt-[160px] md:w-[70%]'>
+    id='faq-section' className='duration-300 transition-all m-auto w-[90%] pt-[200px] md:pt-[100px] lg:pt-[160px] md:w-[70%]'>
       <motion.h4
       variants={fadeIn("right")}
-      className='text-text-color py-3 md:py-12 md:mt-6 lg:py-8 font-bold text-2xl md:text-3xl'>Frequently asked questions</motion.h4>
+      className='text-text-color py-4 md:py-12 md:mt-6 lg:py-8 font-bold text-2xl md:text-3xl'>Frequently asked questions</motion.h4>
    <motion.div className='pb-8'>
     {faqs.map((faq) => (
      <SingleFAQ faq={faq} />
